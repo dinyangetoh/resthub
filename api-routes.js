fcb0b55ddb029b3fc30adcd3a660c58195449075
David Inyang-Etoh <dinyangetoh@gmail.com>
2018-07-01 15:52:00 +0100

    added working API endpoints for contacts

---
+++ api-routes.js
@@ -0,0 +1,27 @@
+// Initialize express router
+let router = require('express').Router();
+
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub crafted with love!',
+    });
+});
+
+// Import contact controller
+var contactController = require('./contactController');
+
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+// Export API routes
+module.exports = router;
